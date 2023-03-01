3647850c986de968d8a06e41f355274bf582d663
Trent Glimp <chitr8or@gmail.com>
2023-03-01 08:10:09 -0600

    Implemented Generics

--- CountPairsAlgorithm/DuplicateCounter.java
+++ CountPairsAlgorithm/DuplicateCounter.java
@@ -3,30 +3,28 @@ package CountPairsAlgorithm;
 import java.util.*;
 
 public class DuplicateCounter {
-    public static int findPairs(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        Set<Integer> duplicates = new HashSet<>();
-        for (int num : nums) {
-            if (!set.add(num)) {
-                duplicates.add(num);
+    public static <Item> int findPairs(Item[] arr) {
+        Set<Item> set = new HashSet<>();
+        Set<Item> duplicates = new HashSet<>();
+        for (Item element : arr) {
+            if (!set.add(element)) {
+                duplicates.add(element);
             }
         }
         return duplicates.size();
     }
 
     public static void main(String[] args) {
-        int[] nums1 = {1, 1, 2, 2, 3, 3};
-        int[] nums2 = {1, 7, 1, 4, 7, 8};
-        int[] nums3 = {1, 2, 2, 3, 4, 6};
-        int[] nums4 = {1, 2, 1, 6, 7, 1};
-        int[] nums5 = {1, 4, 4, 4, 2, 8};
-        int[] nums6 = {2, 2, 2, 2, 2, 2};
+        Integer[] ints = {1, 1, 2, 2, 3, 3};
+        Double[] doubles = {1.1, 1.2, 2.2, 2.2, 3.3, 3.3};
+        String[] strings = {"apple", "banana", "cranberry", "avocado", "banana", "cherry"};
+        Character[] chars = {'a', 'b', 'a', 'c', 'd', 'd'};
+        Boolean[] bools = {true, true, false, false, true, false};
 
-        System.out.println(findPairs(nums1)); // output: 3
-        System.out.println(findPairs(nums2)); // output: 2
-        System.out.println(findPairs(nums3)); // output: 1
-        System.out.println(findPairs(nums4)); // output: 1
-        System.out.println(findPairs(nums5)); // output: 1
-        System.out.println(findPairs(nums6)); // output: 1
+        System.out.println(findPairs(ints)); // output: 3
+        System.out.println(findPairs(doubles)); // output: 2
+        System.out.println(findPairs(strings)); // output: 1
+        System.out.println(findPairs(chars)); // output: 2
+        System.out.println(findPairs(bools)); // output: 2
     }
 }
